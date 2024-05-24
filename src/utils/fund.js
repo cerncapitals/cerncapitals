912ed2eac82a3d746daec931ecdd56364720752a
cerncapitals <cerncapitals@gmail.com>
2024-05-24 21:26:08 +0100

    Update fund.js

--- src/utils/fund.js
+++ src/utils/fund.js
@@ -1,7 +1,6 @@
 import qr1 from '../assets/qr1.jpeg';
 import qr4 from '../assets/qr4.jpeg';
 
-
 export const wallet = [
   {
     id: 1,
@@ -9,7 +8,7 @@ export const wallet = [
     image: qr1,
     network: "BTC",
     address: "bc1q5tn0q2fqazurzk7u6entz5tsazv9fgqtpmuld6",
-    link: "https://cerncapitals.com/dashboard/fund",
+    link: "https://cerncapitals.com/dashboard/fund"
   },
   {
     id: 4,
@@ -17,6 +16,6 @@ export const wallet = [
     image: qr4,
     network: "TRC20",
     address: "TKy1aWK6PaAJ6tZETQ5vfQrdKrpHFj7U2P",
-    link: "https://cerncapitals.com/dashboard/fund",
+    link: "https://cerncapitals.com/dashboard/fund"
   }
 ];
