f68d970777faaf4ff415fab5bc7a7c2e20cb64e9
cerncapitals <cerncapitals@gmail.com>
2024-10-24 22:38:14 +0100

    Update MiningOverview.js

--- src/components/miningOverview/MiningOverview.js
+++ src/components/miningOverview/MiningOverview.js
@@ -1,16 +1,8 @@
 import styles from './MiningOverview.module.css';
-import activeUsers from '../../assets/active_users.svg';
-import overview from '../../assets/mining_chart.svg';
+
 
 export default function MiningOverview() {
   return (
-    <div className={styles.container}>
-      <div className={styles.left}>
-        <img src={activeUsers1} alt="active users"/>
-      </div>
-      <div className={styles.right}>
-        <img src={overview1} alt="mining overview"/>
-      </div>
-    </div>
+   
   )
 }
